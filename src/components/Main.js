ec721020bed78a515d7b17ba2bbe525d1f71580b
0tuedon <tuedonmofe@gmail.com>
2021-11-06 14:17:20 +0100

    Removed Href Attribute

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,21 @@
 import search from '../search.svg'
 import speech from '../speech.svg'
-import side from    '../side.jpg'
-const Main = ({value,word})=>
-{
-    return(
+import side from '../side.jpg'
+const Main = ({ value, word }) => {
+    return (
         <div className="container">
             <div className="img-section">
-            {word.length===0 ?<img src={side} alt="" /> : <h2>{word}</h2>}
+                {word.length === 0 ? <img src={side} alt="" /> : <h2>{word}</h2>}
             </div>
             <form className="img-section" onChange={value}>
-            <label className="form-input">
-            <span className="search"><img src={search} alt="" /></span> <span className="input"><input type="text"  /> </span> <span className="speech"><img src={speech} alt="" /></span>
-            </label>
+                <label className="form-input">
+                    <span className="search"><img src={search} alt="" /></span> <span className="input"><input type="text" /> </span> <span className="speech"><img src={speech} alt="" /></span>
+                </label>
             </form>
             <div className="img-section submit-btn">
                 <button type="submit">Google Search</button> <button>I'm feeling Lucky</button>
             </div>
-            <div className="img-section translation"><span>Google offered in:</span> <a href="#">Deutsch</a> <a href="#">Français</a> <a href="#">Italiano</a> <a href="#">Rumantsch</a>   </div>
+            <div className="img-section translation"><span>Google offered in:</span> <a>Deutsch</a> <a >Français</a> <a >Italiano</a> <a >Rumantsch</a>   </div>
         </div>
     )
 }
