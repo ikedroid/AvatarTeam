a739d75055ba25205caf1d365893c5882a0a7600
emmabraboke <brabokeemma@gmail.com>
2021-11-06 13:25:38 +0100

    initial commti

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,25 @@
-
-const Main = ()=>
+import search from '../search.svg'
+import speech from '../speech.svg'
+import side from    '../side.jpg'
+import { waitForDomChange } from '@testing-library/dom'
+const Main = ({value,word})=>
 {
     return(
-        <div>
-            <h1>The Main</h1>
+        <div className="container">
+            {console.log(word.length)}
+            <div className="img-section">
+            {word.length==0 ?<img src={side} alt="" /> : <h2>{word}</h2>}
+            </div>
+            <form className="img-section" onChange={value}>
+            <label className="form-input">
+            <span className="search"><img src={search} alt="" /></span> <span className="input"><input type="text"  /> </span> <span className="speech"><img src={speech} alt="" /></span>
+            </label>
+            </form>
+            <div className="img-section submit-btn">
+                <button type="submit">Google Search</button> <button>I'm feeling Lucky</button>
+            </div>
+
+            <div className="img-section translation"><span>Google offered in:</span> <a href="#">Deutsch</a> <a href="#">Français</a> <a href="#">Italiano</a> <a href="#">Rumantsch</a>   </div>
         </div>
     )
 }
