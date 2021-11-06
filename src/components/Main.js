82d845ddb2d6661093eea04bef0112c9c1cc1247
0tuedon <tuedonmofe@gmail.com>
2021-11-06 14:10:36 +0100

    Emmanuel's Change

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,12 @@
 import search from '../search.svg'
 import speech from '../speech.svg'
 import side from    '../side.jpg'
-import { waitForDomChange } from '@testing-library/dom'
 const Main = ({value,word})=>
 {
     return(
         <div className="container">
-            {console.log(word.length)}
             <div className="img-section">
-            {word.length==0 ?<img src={side} alt="" /> : <h2>{word}</h2>}
+            {word.length===0 ?<img src={side} alt="" /> : <h2>{word}</h2>}
             </div>
             <form className="img-section" onChange={value}>
             <label className="form-input">
@@ -18,7 +16,6 @@ const Main = ({value,word})=>
             <div className="img-section submit-btn">
                 <button type="submit">Google Search</button> <button>I'm feeling Lucky</button>
             </div>
-
             <div className="img-section translation"><span>Google offered in:</span> <a href="#">Deutsch</a> <a href="#">Français</a> <a href="#">Italiano</a> <a href="#">Rumantsch</a>   </div>
         </div>
     )
